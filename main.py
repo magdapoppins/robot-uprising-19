7891f01536745ccddc2297da3c72e145de1e2a66
Petja Valkama <petja.valkama@futurice.com>
2019-10-11 19:59:52 +0300

    add stuff

--- main.py
+++ main.py
@@ -8,5 +8,30 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
+# Outputs = Port.A, Port.B.... etc
+# Inputs = Port.S1, Port.S2... etc
+#  DOcs https://le-www-live-s.legocdn.com/sc/media/files/ev3-micropython/ev3micropythonv100-71d3f28c59a1e766e92a59ff8500818e.pdf
+
 # Write your program here
+SPEED = 1000000
 brick.sound.beep()
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+wheel_diameter = 56
+axle_track = 114
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+c_sensor = ColorSensor(Port.S1)
+last_ambient = 2000
+while True:
+    c = c_sensor.color()
+    a = c_sensor.ambient()
+    print(a,c == Color.WHITE)
+    if c == Color.WHITE and a > 1:
+        if a < last_ambient:
+            robot.drive(SPEED, 666)
+        robot.drive(SPEED,0)
+        last_ambient = a
+    else:
+        robot.stop()
