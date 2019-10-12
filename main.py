c87c99708da6db3f610bf9d986efc0aa2e21d52c
Sami Nurmivaara <sami@nurmivaara.fi>
2019-10-12 11:43:54 +0300

    Revamped zone logic

--- main.py
+++ main.py
@@ -24,12 +24,11 @@ tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
 # drive in a different turn for 3 seconds
 
 brick.sound.beep()
-areas = ["FOLLOW_LINE", "PRESS_BTN", "FOREST"]
+areas = ["PARKING", "SPINNERS", "FOREST", "BOXZONE", "PRESS_BTN", "MAZE"]
+area = areas.pop()
 
 wheel_diameter = 56
 axle_track = 114
-
-area = areas[0]
 
 c_sensor = ColorSensor(Port.S2)
 last_ambient = 2000
@@ -38,13 +37,15 @@ last_ambient = 2000
 while True:
     c = c_sensor.color()
     if c == Color.RED:
-        area = areas[areas.index[area] + 1]
+        if area == "MAZE":
+            area = areas.pop()
+            print(area)
         tank_drive.on_for_seconds(SpeedPercent(-30), SpeedPercent(30), 0.1)
 
-    if area == areas[0]:
+    if area == "MAZE":
         c = c_sensor.color()
         a = c_sensor.ambient()
-        print(a, c == Color.WHITE)
+        #print(a, c == Color.WHITE)
         if c == Color.WHITE and a > 1:
             if a < last_ambient:
                 tank_drive.on_for_seconds(
@@ -52,8 +53,3 @@ while True:
         else:
             tank_drive.on_for_seconds(SpeedPercent(-20), SpeedPercent(20), 0.1)
         last_ambient = a
-    elif area == areas[1]:
-        print("asd")
-    elif area == areas[2]:
-        print("asd")
-        
