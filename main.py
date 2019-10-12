0f1c2db96343e164c1ed0b4fdce74d6ebc3cbf6d
Petja Valkama <petja.valkama@futurice.com>
2019-10-12 18:42:16 +0300

    Add threading

--- main.py
+++ main.py
@@ -4,33 +4,50 @@ from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C
 from roina import Claw, Movement, Controller, Linefollower, BoxZone
 import sys
 import logging
+from threading import Thread
 
 # Outputs = Port.A, Port.B.... etc
 # Inputs = Port.S1, Port.S2... etc
 # DOcs https://le-www-live-s.legocdn.com/sc/media/files/ev3-micropython/ev3micropythonv100-71d3f28c59a1e766e92a59ff8500818e.pdf
 
 # Write your program here
-brick.sound.beep()
-theEnd = false
-while not theEnd:
-  move = Movement(OUTPUT_A, OUTPUT_B, Port.S3, Port.S2)
-  # pre-stuff: close claws for usb stick
-  grabber = Claw(OUTPUT_C)
-  grabber.down()
 
-  # first section MAZE
-  follower = Linefollower(Port.S2, Port.S3, OUTPUT_A, OUTPUT_B) 
-  follower.followLine()
 
-  # go to next section
-  move.driveUntilColorEnds(Color.RED)
+class Main(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
 
-  # second section PRESS_BTN
-  pressButton = pressbtn(Port.S2, Port.S3, OUTPUT_A, OUTPUT_B, OUTPUT_C)
-  pressButton.pressButton()
+    def run(self):
+        brick.sound.beep()
+        theEnd = false
+        while not theEnd:
+            move = Movement(OUTPUT_A, OUTPUT_B, Port.S3, Port.S2)
+            # pre-stuff: close claws for usb stick
+            grabber = Claw(OUTPUT_C)
+            grabber.down()
 
-  # go to next section
-  move.driveUntilColorEnds(Color.RED)
+            # first section MAZE
+            follower = Linefollower(Port.S2, Port.S3, OUTPUT_A, OUTPUT_B)
+            follower.followLine()
 
-  # third section BOXZONE
-  boxzone = BoxZone(Port.S2, OUTPUT_A, OUTPUT_B, OUTPUT_C)
+            # go to next section
+            move.driveUntilColorEnds(Color.RED)
+
+            # second section PRESS_BTN
+            pressButton = pressbtn(
+                Port.S2, Port.S3, OUTPUT_A, OUTPUT_B, OUTPUT_C)
+            pressButton.pressButton()
+
+            # go to next section
+            move.driveUntilColorEnds(Color.RED)
+
+            # third section BOXZONE
+            boxzone = BoxZone(Port.S2, OUTPUT_A, OUTPUT_B, OUTPUT_C)
+
+
+Main()
+Controller()
+while True:
+    pass
--- roina/controller.py
+++ roina/controller.py
@@ -1,12 +1,19 @@
 import evdev
+from threading import Thread
+import logging
 
 
-class Controller:
+class Controller(Thread):
     def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
         self.devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
         self.gamepad = evdev.InputDevice(self.devices[0].fn)
 
+    def run(self):
         for event in self.gamepad.read_loop():
-            print(event.type)
-            print(event.code)
-            print('---')
+            logging.info('controller input:')
+            logging.info('type: ' + event.type)
+            logging.info('code: ' + event.code)
+            logging.info('---')
